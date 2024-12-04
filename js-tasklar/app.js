07fcde730fcbf6549544c6570fa6c06d1088b1ce
Miryehya <miryehya949@gmail.com>
2024-12-04 16:46:03 +0400

    first commit

---
+++ js-tasklar/app.js
@@ -0,0 +1,67 @@
+//const Number = prompt("reqem daxil et");
+//let cem = 0;
+//for (let i = 1; i < Number; i++) {
+//  cem += i;
+//}
+//document.write(`1den ${Number} ededine  qeder butun reqemlerin cemi ${cem}`);
+//
+
+//------------------------------------------------
+//
+//const Number = prompt("reqem daxil et");
+//let hasil = 1;
+//for (let i = 1; i <= Number; i++) {
+//  hasil *= i;
+//}
+//document.write(`1den ${Number} ededine  qeder butun reqemlerin hasili ${hasil}`);
+
+//------------------------------------------------------
+//
+//const Number = 23;
+//for (let i = Number; i <= 106; i += 7) {
+//    console.log(i);
+//}
+//----------------------------------------------------------
+//
+//let Number = prompt("reqem daxil et nece defe yazi yazilsin");
+//i = 1;
+//while( i <= Number ){
+//i++;
+//console.log("i know how to use cycles");
+//}
+
+//-------------------------------------------------------------------
+
+//for (let i = 100; i <= 990; i++) {
+//  if (!(i % 10)) {
+//    console.log(i);
+//  }
+//}
+//------------------------------------------------------------------
+//let cem = 0;
+//for (let i = 11; i <= 99; i++) {
+//  if (i % 2 === 1) {
+//    cem += i;
+//  }
+//}
+//console.log(cem);
+
+//-----------------------------------------------------------------------
+//const n = prompt("reqem daxil edin");
+//let cem = 0;
+//for (let i = 100; i <= 999; i++) {
+//  if (!(i % n)) {
+//    cem += i;
+//  }
+//}
+//
+//console.log(cem);
+//------------------------------------------------------------------------
+//
+//const n = 16;
+//for (let i = n; i > 0; i--) {
+//  if (i % 2) {
+//    console.log(i);
+//  }
+//}
+//---------------------------------------------------------------------------
